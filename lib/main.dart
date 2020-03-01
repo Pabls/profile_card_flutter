dea50d6fc5e899d5d4b539478ba7dc8640316711
PS <pabls@yandex.ru>
2020-03-01 17:51:59 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 16,
+            ),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/avatar.jpg'),
+            ),
+            Text(
+              "Capybara",
+              style: TextStyle(
+                  fontFamily: "Pacifico",
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Source Sans Pro"),
+            ),
+            Divider(
+              color: Colors.white,
+            ),
+            Card(
+
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("+7 111 222 33 44",
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontSize: 20,
+                            fontFamily: "Source Sans Pro"))
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("capybara@capybara.com",
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontSize: 20,
+                            fontFamily: "Source Sans Pro"))
+                  ],
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
